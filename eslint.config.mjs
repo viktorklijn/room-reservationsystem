0f7cac48399e008ce4f800e4220aa0bc649b1d21
Viktor Klijn <viktor.klijn@student.hu.nl>
2025-05-13 10:06:22 +0200

    eslint errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,19 @@
-import { defineConfig } from "eslint/config";
-import globals from "globals";
-import js from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
+import { FlatCompat } from "@eslint/eslintrc";
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"] },
-  {
-    files: ["**/*.{js,mjs,cjs,jsx}"],
-    languageOptions: { globals: globals.browser },
-  },
-  {
-    files: ["**/*.{js,mjs,cjs,jsx}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-  },
-  pluginReact.configs.flat.recommended,
-]);
+const compat = new FlatCompat({
+  // import.meta.dirname is available after Node.js v20.11.0
+  baseDirectory: import.meta.dirname,
+});
+
+const eslintConfig = [
+  ...compat.config({
+    extends: ["next"],
+    settings: {
+      next: {
+        rootDir: "packages/my-app/",
+      },
+    },
+  }),
+];
+
+export default eslintConfig;
